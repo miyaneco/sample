236275dc7b31cfabe8bd53aca5ee7b9bcbce2d13
miyahara <miya.neco211gmail.com>
2020-08-18 14:31:21 +0900

    整数のいくつかの演算を勉強した

--- sample01.py
+++ sample01.py
@@ -5,10 +5,18 @@ print('Tom')
 # コメント
 # 最初は勉強メモ的に使ってもらうといい
 #SyntaxError　文法エラー　構文エラー
+
 #わざとエラーを起こそう！
 #print('hello'
-#わざとエラー
 
-print('hello)
-#最後の綴じカッコが全角
-print('hello'）
+#わざとエラー
+#print('hello)
+
+#最後の閉じカッコが全角
+#print('hello'）
+
+#整数の基本的な演算
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
