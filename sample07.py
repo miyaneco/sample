4714ebf0839f1966ce752acb88856a9b080e6402
miyahara <miya.neco211gmail.com>
2020-08-18 16:31:59 +0900

    条件分岐２　３

---
+++ sample07.py
@@ -0,0 +1,19 @@
+# 条件分岐２
+
+# score = 60
+#
+# if score >= 70:
+#     print('Great')
+# else:
+#     print('So so')
+
+ # 条件分岐3 if elif else
+
+score = 30
+
+if score >= 70:
+    print('Great')
+elif score >= 40:
+        print('So so')
+else:
+    print('Bad...')
